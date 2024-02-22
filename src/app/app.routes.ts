da1460740a152703b984da61a3908eaeac315b67
gustavo-paipe <gustavorafaeldecarvalho@gmail.com>
2024-02-22 20:10:59 -0300

    add: grid style and functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
 import { Routes } from '@angular/router';
 import { ButtonComponent } from '../components/button-awesome/button.component';
+import { Grid } from '../components/grid/grid';
 
 export const routes: Routes = [
   {
     path: 'cards',
-    component: ButtonComponent,
+    component: Grid,
   },
   {
     path: 'buttons',
-    component: ButtonComponent,
+    component: Grid,
   },
   {
     path: 'buttons/background-circle-motion-button',
---
+++ src/components/grid/grid.ts
@@ -0,0 +1,51 @@
+import { Location } from '@angular/common';
+import { Component } from '@angular/core';
+
+interface CardsList {
+  label: string;
+  src: string;
+}
+interface MenuList {
+  title: string;
+  cards: Array<CardsList>;
+}
+
+const listMenu = [
+  {
+    title: 'buttons',
+    cards: [
+      {
+        label: 'background-circle-motion-button',
+        src: '../../assets/images/background-circle-motion-button.png',
+      },
+    ],
+  },
+  {
+    title: 'cards',
+    cards: [
+      {
+        label: 'background-circle-motion-button',
+        src: '../../assets/images/background-circle-motion-button.png',
+      },
+      {
+        label: 'background-circle-motion-button',
+        src: '../../assets/images/background-circle-motion-button.png',
+      },
+    ],
+  },
+];
+
+@Component({
+  selector: 'grid',
+  standalone: true,
+  templateUrl: './grid.html',
+})
+export class Grid {
+  title = 'grid';
+  constructor(private location: Location) {}
+
+  pageLabel: string = this.location.path().split('/')[1];
+  list: Array<CardsList> =
+    listMenu[listMenu.findIndex((value) => value.title === this.pageLabel)]
+      .cards;
+}
